c7a3587d4d9dca8c9ca8e69e8f8f6bd35f095d41
louis.y <louis.y.2.tcg@gmail.com>
2024-09-29 17:48:18 +0800

    chore: init role table's date

---
+++ user/02.patch/init-role-date.sql
@@ -0,0 +1,4 @@
+INSERT INTO SPL_MEMBER.ROLE (ROLE_ID, ROLE_TYPE, DESCRIPTION) VALUES
+(1, 'NORMAL_USER', 'normal user'),
+(2, 'COLLAB_USER', 'collaboration partner'),
+(3, 'ADMIN', 'administrator');
